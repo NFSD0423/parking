90f3229c2eb2681850740cc18bb8466f6cd2798b
Fernando <fernando.calvo.sanz@gmail.com>
2023-05-08 22:58:35 +0200

    Store places in array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const carsLimit = 10
+const bikesLimit = 5
+
+const initCarsParking = new Array(carsLimit).fill(false)
 function App() {
+  const [carsParking, setCarsParking] = useState(initCarsParking)
+
+  const addCar = (spaceNum) => {
+    carsParking[spaceNum] = true
+    setCarsParking([...carsParking])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Parking de coches</h1>
+      <div>
+        {carsParking.map((filled, i) => {
+          return (
+            <div>
+              Plaza {i}
+              {filled ? "Ocupada" : "Libre"}
+              <button onClick={() => {
+                addCar(i)
+              }}>Aparcar</button>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
